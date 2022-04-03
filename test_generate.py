9c5bbf28485e31bfefa2c7fb13ad7362f411d8ed
Gabor Szabo <gabor@szabgab.com>
2022-04-03 17:18:54 +0300

    parametrize the test

--- test_generate.py
+++ test_generate.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+import pytest
 
 from librelingo_yaml_loader.yaml_loader import load_course
 from generate import load_dictionary, collect_data, export_json
@@ -9,26 +10,27 @@ from generate import load_dictionary, collect_data, export_json
 data_path = 'ladino-diksionaryo-data/words'
 path_to_course = 'LibreLingo-Judeo-Spanish-from-English/course'
 
-def test_andjinara(tmpdir):
+@pytest.mark.parametrize("name", ['andjinara'])
+def test_one_file(tmpdir, name):
     print(tmpdir)
-    shutil.copy(os.path.join(data_path, 'andjinara.yaml'), os.path.join(tmpdir, 'andjinara.yaml'))
+    shutil.copy(os.path.join(data_path, f'{name}.yaml'), os.path.join(tmpdir, f'{name}.yaml'))
     course = load_course(path_to_course)
     dictionary_source = load_dictionary(tmpdir)
     #print(dictionary_source)
-    assert dictionary_source == [{
-        'ladino': 'andjinara',
-        'accented': 'andjinára',
-        'gender': 'feminine',
-        'number': 'singular',
-        'alternative-spelling': [{'ladino': 'endjinara', 'accented': 'endjinára'}],
-        'translations': {
-            'english': 'artichoke',
-            'french': 'artichaut',
-            'hebrew': 'ארטישוק',
-            'spanish': ['alcaucil', 'alcachofa'],
-            'turkish': 'enginar'
-        }
-    }]
+    #assert dictionary_source == [{
+    #    'ladino': 'andjinara',
+    #    'accented': 'andjinára',
+    #    'gender': 'feminine',
+    #    'number': 'singular',
+    #    'alternative-spelling': [{'ladino': 'endjinara', 'accented': 'endjinára'}],
+    #    'translations': {
+    #        'english': 'artichoke',
+    #        'french': 'artichaut',
+    #        'hebrew': 'ארטישוק',
+    #        'spanish': ['alcaucil', 'alcachofa'],
+    #        'turkish': 'enginar'
+    #    }
+    #}]
     target, source, dictionary, count = collect_data(course, dictionary_source)
     export_json(
         dictionary, "dictionary.json", tmpdir
@@ -41,10 +43,10 @@ def test_andjinara(tmpdir):
     #print(source)
     #print(dictionary)
     #print(count)
-    with open (os.path.join('tests', 'andjinara', 'dictionary.json')) as fh:
+    with open (os.path.join('tests', name, 'dictionary.json')) as fh:
         expected = json.load(fh)
     assert dictionary == expected
-    with open (os.path.join('tests', 'andjinara', 'count.json')) as fh:
+    with open (os.path.join('tests', name, 'count.json')) as fh:
         expected_count = json.load(fh)
     assert count == expected_count
 
