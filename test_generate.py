997c948f5b29301fb1b17598a0b6bdc38760a05a
Gabor Szabo <gabor@szabgab.com>
2022-04-04 10:52:13 +0300

    add the --save flag to pytest to make it easy to replace the expected files

--- test_generate.py
+++ test_generate.py
@@ -11,16 +11,20 @@ data_path = 'ladino-diksionaryo-data/words'
 path_to_course = 'LibreLingo-Judeo-Spanish-from-English/course'
 
 @pytest.mark.parametrize("name", ['andjinara', 'komer'])
-def test_one_file(tmpdir, name):
+def test_one_file(tmpdir, request, name):
     print(tmpdir)
     shutil.copy(os.path.join(data_path, f'{name}.yaml'), os.path.join(tmpdir, f'{name}.yaml'))
     course = load_course(path_to_course)
     dictionary_source = load_dictionary(tmpdir)
     target, source, dictionary, count = collect_data(course, dictionary_source)
     # export in case we would like to update the files in the tests/ directory
-    export_json(dictionary_source, os.path.join(tmpdir, "dictionary_source.json"), pretty=True)
-    export_json(dictionary, os.path.join(tmpdir, "dictionary.json"), pretty=True)
-    export_json(count, os.path.join(tmpdir, "count.json"), pretty=True)
+    if request.config.getoption("--save"):
+        path = os.path.join('tests', name)
+    else:
+        path = tmpdir
+    export_json(dictionary_source, os.path.join(path, "dictionary_source.json"), pretty=True)
+    export_json(dictionary, os.path.join(path, "dictionary.json"), pretty=True)
+    export_json(count, os.path.join(path, "count.json"), pretty=True)
 
     with open (os.path.join('tests', name, 'dictionary_source.json')) as fh:
         expected_source = json.load(fh)
