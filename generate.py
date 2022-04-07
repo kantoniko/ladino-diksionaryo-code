6d1ec52720b63d3257f12b398374939992ec6510
Gabor Szabo <gabor@szabgab.com>
2022-04-07 16:56:10 +0300

    make translations unique

--- generate.py
+++ generate.py
@@ -385,7 +385,7 @@ def _add_ladino_word(ladino_word, accented_word, dictionary, pages, entry):
             dictionary['ladino'][ladino_word][language].extend(words)
         else:
             raise Exception("bad")
-        dictionary['ladino'][ladino_word][language].sort()
+        dictionary['ladino'][ladino_word][language] = sorted(set(dictionary['ladino'][ladino_word][language]))
 
     if ladino_word not in pages['ladino']:
         pages['ladino'][ladino_word] = []
@@ -403,7 +403,7 @@ def _add_ladino_word(ladino_word, accented_word, dictionary, pages, entry):
             dictionary['ladino'][ladino_word][language].extend(words)
         else:
             raise Exception("bad")
-    dictionary['ladino'][ladino_word][language].sort()
+    dictionary['ladino'][ladino_word][language] = sorted(set(dictionary['ladino'][ladino_word][language]))
 
 def _add_translated_words(language, dictionary, pages, entry, count):
     translations = entry['translations'].get(language)
