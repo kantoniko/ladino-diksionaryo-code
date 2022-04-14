5194099fcf25ab1faa6765c5b2fbc70aae51ab6b
Gabor Szabo <gabor@szabgab.com>
2022-04-14 08:27:25 +0300

    make the bulma navbar hamburger work

---
+++ ladino/js/all.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $(".navbar-burger").click(function() {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+  });
+});
