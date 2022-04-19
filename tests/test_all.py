7ac5e802d26547ca804cdaccd62ed817daa7030c
Gabor Szabo <gabor@szabgab.com>
2022-04-19 16:26:11 +0300

    use the main() function in the all test as well

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,11 +1,9 @@
-from ladino.generate import load_dictionary, collect_data, export_dictionary_pages, export_dictionary_lists, load_config
+from ladino.generate import main
+import sys
 
 repo_path = 'ladino-diksionaryo-data'
-data_path = 'ladino-diksionaryo-data/words'
 
 def test_all(tmpdir):
     print(tmpdir)
-    dictionary_source, all_examples = load_dictionary(load_config(repo_path), data_path)
-    dictionary, count, pages = collect_data(dictionary_source)
-    export_dictionary_pages(pages, tmpdir)
-    export_dictionary_lists(pages, tmpdir)
+    sys.argv = [sys.argv[0], '--all', '--html',  str(tmpdir), '--dictionary', repo_path]
+    main()
