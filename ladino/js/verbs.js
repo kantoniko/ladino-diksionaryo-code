acaa492c24e06f57e407b4ba73c6025819754e22
Gabor Szabo <gabor@szabgab.com>
2022-04-29 18:11:30 +0300

    nodejs implementation of the conjugation if -er

---
+++ ladino/js/verbs.js
@@ -0,0 +1,76 @@
+function conjugations(verb) {
+    let data = {
+        "to": verb
+    };
+    if (verb.endsWith('er')) {
+        let root = verb.slice(0, -2);
+        data['prezente'] = {
+            'yo': root + 'o',
+            'tu': root + 'es',
+            'el': root + 'e',
+            'moz': root + 'emos',
+            'voz': root + 'ésh',
+            'eyos': root + 'en',
+        }
+        data['imperfekto'] = {
+            'yo': root + 'ía',
+            'tu': root + 'ías',
+            'el': root + 'ía',
+            'moz': root + 'íamos',
+            'voz': root + 'íash',
+            'eyos': root + 'ían',
+        }
+        data['pasado'] = {
+            'yo': root + 'í',
+            'tu': root + 'ites',
+            'el': root + 'ió',
+            'moz': root + 'imos',
+            'voz': root + 'itesh',
+            'eyos': root + 'ieron',
+        }
+        data['futuro'] = {
+            'yo': root + 'eré',
+            'tu': root + 'erás',
+            'el': root + 'erá',
+            'moz': root + 'eremos',
+            'voz': root + 'erésh',
+            'eyos': root + 'erán',
+        }
+        data['subjunktivo'] = {
+            'yo': root + 'a',
+            'tu': root + 'as',
+            'el': root + 'a',
+            'moz': root + 'amos',
+            'voz': root + 'ásh',
+            'eyos': root + 'an',
+        }
+        data['subjunktivo_imperfekto'] = {
+            'yo': root + 'iera',
+            'tu': root + 'ieras',
+            'el': root + 'iera',
+            'moz': root + 'iéramos',
+            'voz': root + 'ierash',
+            'eyos': root + 'ieran',
+        }
+        data['kondisional'] = {
+            'yo': root + 'ería',
+            'tu': root + 'erías',
+            'el': root + 'ería',
+            'moz': root + 'eríamos',
+            'voz': root + 'eríash',
+            'eyos': root + 'erían',
+        }
+        data['imperativo'] = {
+            'tu': root + 'e',
+            'el': root + 'as',
+            'voz': root + 'ed',
+            'eyos': root + 'ásh',
+        }
+        data['infinitivo'] = root + "er",
+        data['djerundivo'] = root + "iendo",
+        data['partisipio_pasado'] = root + "ido"
+    }
+
+    return data
+}
+module.exports.conjugations = conjugations;
---
+++ tests/test_verbs.js
@@ -0,0 +1,76 @@
+assert = require('assert');
+const verbs = require('.././ladino/js/verbs');
+let res = verbs.conjugations("komer");
+
+komer = {
+    to: "komer",
+    prezente: {
+      yo: 'komo',
+      tu: 'komes',
+      el: 'kome',
+      moz: 'komemos',
+      voz: 'komésh',
+      eyos: 'komen'
+    },
+    imperfekto: {
+      yo: 'komía',
+      tu: 'komías',
+      el: 'komía',
+      moz: 'komíamos',
+      voz: 'komíash',
+      eyos: 'komían'
+    },
+    pasado: {
+      yo: 'komí',
+      tu: 'komites',
+      el: 'komió',
+      moz: 'komimos',
+      voz: 'komitesh',
+      eyos: 'komieron'
+    },
+    futuro: {
+      yo: 'komeré',
+      tu: 'komerás',
+      el: 'komerá',
+      moz: 'komeremos',
+      voz: 'komerésh',
+      eyos: 'komerán'
+    },
+    subjunktivo: {
+      yo: 'koma',
+      tu: 'komas',
+      el: 'koma',
+      moz: 'komamos',
+      voz: 'komásh',
+      eyos: 'koman'
+    },
+    subjunktivo_imperfekto: {
+      yo: 'komiera',
+      tu: 'komieras',
+      el: 'komiera',
+      moz: 'komiéramos',
+      voz: 'komierash',
+      eyos: 'komieran'
+    },
+    kondisional: {
+      yo: 'komería',
+      tu: 'komerías',
+      el: 'komería',
+      moz: 'komeríamos',
+      voz: 'komeríash',
+      eyos: 'komerían'
+    },
+    imperativo: {
+      tu: 'kome',
+      el: 'komas',
+      voz: 'komed',
+      eyos: 'komásh'
+    },
+    infinitivo: 'komer',
+    djerundivo: 'komiendo',
+    partisipio_pasado: 'komido'
+};
+//console.log(res)
+
+assert.deepEqual(res, komer);
+
