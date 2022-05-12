020aed2b241e6ea8e78506cf7b104a5c6e8fce35
Gabor Szabo <gabor@szabgab.com>
2022-05-12 14:22:32 +0300

    sort sitemap

--- ladino/export.py
+++ ladino/export.py
@@ -394,7 +394,7 @@ def create_sitemap(html_dir):
     xml = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 '''
-    for entry in sitemap:
+    for entry in sorted(sitemap):
         xml += f'''<url><loc>https://diksionaryo.szabgab.com/{entry}</loc></url>\n'''
     xml += '</urlset>'
     with open(os.path.join(html_dir, 'sitemap.xml'), 'w') as fh:
