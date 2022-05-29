587c986352ef42dbc61250f870be9883e7387494
Gabor Szabo <gabor@szabgab.com>
2022-05-29 12:27:14 +0300

    warn about the accented being the same as the ladion word

--- ladino/load.py
+++ ladino/load.py
@@ -144,6 +144,11 @@ def load_dictionary(config, path_to_dictionary):
         for version in data['versions']:
             if 'ladino' not in version:
                 raise LadinoError(f"The ladino 'version' is missing from file '{filename}'")
+
+            if 'accented' in version and version['accented'] == version['ladino']:
+                print(f"The accented is the same as the ladino in '{filename}'")
+                #raise LadinoError(f"The accented is the same as the ladino in '{filename}'")
+
             version['source'] = filename
 
             if 'translations' in version:
