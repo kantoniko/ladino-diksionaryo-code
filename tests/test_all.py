1d7301390af396293dce541d9544ff8716916eed
Gabor Szabo <gabor@szabgab.com>
2022-07-22 15:10:30 +0200

    make the test_all run locally again

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,11 +2,16 @@ from ladino.generate import main
 import sys
 import os
 
-repo_path = 'ladino-diksionaryo-data'
+for real_repo_path  in ['ladino-diksionaryo-data', '../ladino-diksionaryo-data']:
+    if os.path.exists(real_repo_path):
+        break
+else:
+    raise Exception("Could not find path to ladino-diksionaryo-data")
+
 
 def test_all(tmpdir):
     print(tmpdir)
-    sys.argv = [sys.argv[0], '--all', '--html',  str(tmpdir), '--dictionary', repo_path]
+    sys.argv = [sys.argv[0], '--all', '--html',  str(tmpdir), '--dictionary', real_repo_path]
     main()
     os.environ["LADINO_DIR"] = str(tmpdir)
     assert os.system("node tests/test_verbs.js") == 0
