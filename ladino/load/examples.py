1363481d15538ad7251d97354d9ce7984b3c6694
Gabor Szabo <gabor@szabgab.com>
2023-01-24 15:52:55 +0200

    add logging

--- ladino/load/examples.py
+++ ladino/load/examples.py
@@ -1,12 +1,15 @@
 import os
 from yaml import safe_load
+import logging
 
 from ladino.common import words_to_url
 
 def load_examples(path_to_examples):
+    logging.info(f"load_examples({path_to_examples})")
     extra_examples = []
     if os.path.exists(path_to_examples):
         for filename in os.listdir(path_to_examples):
+            logging.info(f"load_examples from '{filename}'")
             with open(os.path.join(path_to_examples, filename)) as fh:
                 examples = safe_load(fh)
             #print(examples)
