6156219191c9dda1184f4d4cd5524d7a8b0fbf66
Gabor Szabo <gabor@szabgab.com>
2023-02-01 22:42:30 +0200

    remove the tests that works on the whole live data; we are going to generate the site anyway and it will fail if it is broken

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,12 +1,5 @@
-from ladino.generate import main
-import sys
 import os
-from conftest import data_repo_path
 
 
-def test_all(tmpdir):
-    print(tmpdir)
-    sys.argv = [sys.argv[0], '--all', '--html',  str(tmpdir), '--dictionary', data_repo_path()]
-    main()
-    os.environ["LADINO_DIR"] = str(tmpdir)
+def test_js():
     assert os.system("node tests/test_verbs.js") == 0
