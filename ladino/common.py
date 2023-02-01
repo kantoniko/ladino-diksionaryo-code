6d0c7e9de6ffd774f8d21f60ee201d3dfa7def69
Gabor Szabo <gabor@szabgab.com>
2023-02-01 17:37:10 +0200

    set max file-length to be 50

--- ladino/common.py
+++ ladino/common.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 class LadinoError(Exception):
     pass
@@ -9,6 +10,14 @@ def words_to_url(words):
     plain = re.sub(r'[^a-z0-9]', ' ', words.lower())
     plain = plain.strip()
     plain = re.sub(r'\s+', '-', plain)
+    # logging.info(f"path: {plain}")
+
+    # Make sure the path is not too long and ends with a full word.
+    if len(plain) > 50:
+        plain = plain[0:50]
+        plain = plain[:plain.rindex('-')]
+    # logging.info(f"path: {plain}")
+
     return plain
 
 
