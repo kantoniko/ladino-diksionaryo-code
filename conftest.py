1155b536e809b66648b60075cae6ebf1aa4b7f43
Gabor Szabo <gabor@szabgab.com>
2023-02-05 12:08:58 +0200

    no need to get repo path; everything should be local

--- conftest.py
+++ conftest.py
@@ -4,11 +4,4 @@ def pytest_addoption(parser):
     parser.addoption("--save", action='store_true')
 
 
-def data_repo_path():
-    for real_repo_path  in ['ladino-diksionaryo-data', '../ladino-diksionaryo-data']:
-        if os.path.exists(real_repo_path):
-            break
-    else:
-        raise Exception("Could not find path to ladino-diksionaryo-data")
-    return real_repo_path
 
