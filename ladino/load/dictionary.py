01085a76799e8cb7b3202dbe2630cd041a2efb7f
Gabor Szabo <gabor@szabgab.com>
2023-02-06 09:13:39 +0200

    check gender for pronouns

--- ladino/load/dictionary.py
+++ ladino/load/dictionary.py
@@ -61,9 +61,10 @@ def check_and_collect_grammar(config, data, dictionary, filename):
     if grammar != 'verb' and 'conjugations' in data:
         raise LadinoError(f"Grammar is NOT a 'verb', but there are conjugations in '{filename}'")
 
-    if grammar in ['noun']: # 'adjective',
-        for version in data['versions']:
-            gender = version.get('gender')
+    for version in data.get('versions', []):
+        gender = version.get('gender')
+        # print(gender)
+        if grammar in ['noun', 'pronoun']: # 'adjective',
             if gender is None:
                 raise LadinoError(f"The 'gender' field is None in '{filename}' version {version}")
             if gender not in config['gender']:
@@ -73,6 +74,9 @@ def check_and_collect_grammar(config, data, dictionary, filename):
                 raise LadinoError(f"The 'number' field is None in '{filename}' version {version}")
             if number not in config['numero']:
                 raise LadinoError(f"The 'number' field is '{number}' in '{filename}' version {version}")
+        # else:
+        #     if gender is not None:
+        #         raise LadinoError(f"Unnecessary 'gender' field in '{filename}' version {version}")
 
     dictionary.gramer[grammar].append(data)
 
