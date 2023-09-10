cf585adf612ef57b53d88f53d05a9e933fe7af7c
Gabor Szabo <gabor@szabgab.com>
2023-09-10 17:37:19 +0300

    extract the from_ladino function

--- ladino/js/ladino.js
+++ ladino/js/ladino.js
@@ -14,37 +14,27 @@ function translate(text, languages, dictionary) {
 
 function translate_word(original_word, languages, dictionary) {
     //console.log(word);
-        const word = original_word.toLowerCase();
+    const word = original_word.toLowerCase();
 
-    let source_language = 'ladino';
-    let dictionary_word = dictionary['ladino'][word];
-
-    let ladino_from_source_language = null;
-    if (! dictionary_word) {
-        // console.log(`try accented '${word}'`);
-        // console.log(dictionary["accented"]);
-        ladino_from_source_language = dictionary["accented"][word];
-        // console.log("ladino_from_source_language:", ladino_from_source_language);
-        if (ladino_from_source_language) {
-            dictionary_word = ladino_from_source_language[0];
-            // console.log(dictionary_word);
-        }
+    let response = from_ladino(word, dictionary);
+    if (response["dictionary_word"]) {
+        response["original_word"] = original_word;
+        return response;
     }
 
-
-    if (! dictionary_word) {
-        // console.log(`try '${word}' in translations`);
-        for (var jx=0; jx < languages.length; jx++) {
-            //console.log("check language:", languages[jx]);
-            source_language = languages[jx];
-            ladino_from_source_language = dictionary[source_language][word];
-            //console.log('ladino', ladino_from_source_language);
-            if (ladino_from_source_language) {
-                // TODO: shall we include the dictionary entry of all the words?
-                // TODO: should be select a different one not necessarily the first one?
-                dictionary_word = dictionary['ladino'][ladino_from_source_language[0]];
-                break;
-            }
+    let source_language = 'ladino';
+    let dictionary_word = '';
+    // console.log(`try '${word}' in translations`);
+    for (var jx=0; jx < languages.length; jx++) {
+        //console.log("check language:", languages[jx]);
+        source_language = languages[jx];
+        ladino_from_source_language = dictionary[source_language][word];
+        //console.log('ladino', ladino_from_source_language);
+        if (ladino_from_source_language) {
+            // TODO: shall we include the dictionary entry of all the words?
+            // TODO: should be select a different one not necessarily the first one?
+            dictionary_word = dictionary['ladino'][ladino_from_source_language[0]];
+            break;
         }
     }
 
@@ -57,6 +47,29 @@ function translate_word(original_word, languages, dictionary) {
         'ladino_from_source_language': ladino_from_source_language
     }
 }
+
+function from_ladino(word, dictionary) {
+    let dictionary_word = dictionary['ladino'][word];
+    let ladino_from_source_language = null;
+
+    if (! dictionary_word) {
+        // console.log(`try accented '${word}'`);
+        // console.log(dictionary["accented"]);
+        ladino_from_source_language = dictionary["accented"][word];
+        // console.log("ladino_from_source_language:", ladino_from_source_language);
+        if (ladino_from_source_language) {
+            dictionary_word = ladino_from_source_language[0];
+            // console.log(dictionary_word);
+        }
+    }
+    return {
+        'source_language': 'ladino',
+        'dictionary_word': dictionary_word,
+        'word': word,
+        'ladino_from_source_language': ladino_from_source_language,
+    }
+}
+
 
 function lookup(text, languages, dictionary) {
     const cleaned = text.replace(/[<>,;.:!?"'\n*()=\[\]\/\s]/g, " ");
